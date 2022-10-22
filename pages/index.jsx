ee1eb31ec4f7bf06fa9c07132844337bf486baaa
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-10-22 10:09:11 -0300

    Adicionado Post no index e imagens

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,14 +2,16 @@ import ContainerPage from '../src/components/layout/container/ContainerPage'
 import SearchInput from '../src/components/search/SearchInput'
 import Button from '../src/components/button/ButtonAdd'
 import Body from '../src/components/layout/body/Body'
+import Post from '../src/components/cards/Post'
 
 function HomePage() {
   return (
     <>
       <Body>
         <ContainerPage>
-          <SearchInput></SearchInput>
+          <SearchInput />
           <Button onClick="OnClick">ADICIONAR</Button>
+          <Post />
         </ContainerPage>
       </Body>
     </>
--- src/components/iconImage/IconImages.jsx
+++ src/components/iconImage/IconImages.jsx
@@ -1,4 +1,4 @@
-export default function IconImages({ imageName, type }) {
+export default function IconImages({ imageName, size, type }) {
   // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
-  return <img src={`/images/${imageName}.${type}`} />
+  return <img src={`/images/${imageName}.${type}`} width={size} />
 }
